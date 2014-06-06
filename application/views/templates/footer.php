c1f722090011ed88d5caa75accfa9132bbfe486d
lasdjs <lenninlasd@gmail.com>
2014-06-05 21:15:43 -0500

    cambios rapidos landing

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,10 +1,10 @@
-		<footer>
+<!-- 		<footer>
 			<div class="row">
 				<div class="col-md-12">
 					<div class="text-muted" ><small>Copyright &copy; 2013 <?php echo $lasd;?></small></div>
 				</div>
 			</div>
-		</footer>
+		</footer> -->
 		
 		
 		<script src="<?php  echo base_url("bootstrap/js/bootstrap.min.js"); ?>"></script>
