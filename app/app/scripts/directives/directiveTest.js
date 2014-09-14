b31e826abadc0e005e0122e3eab8a06b12fee999
lasdjs <lenninlasd@gmail.com>
2014-09-14 14:56:18 -0500

    creacion componente navbar

--- app/app/scripts/directives/directiveTest.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('AppDirectiveTest', [])
-	.directive('lsHeader', function(){
-		return{
-			restrict: 'E',
-			templateUrl: "views/header.html"
-		};	
-	});
---
+++ app/app/scripts/directives/navbar.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('AppDirectiveTest', [])
+	.directive('lsHeader', function(){
+
+		function link(scope, element, attrs){
+			
+			scope.compararURL = function(url){
+
+				if (location.hash === url) {
+					return true;
+				}else{
+					return false;
+				}
+			}
+		};
+		return{
+			restrict: 'E',			
+			templateUrl: "views/navbar.html",
+			link: link
+		};	
+	})
+	.controller('header1', ['$scope','$location', function($scope){
+		$scope.datos = {
+			id: 1,
+			nombrePrincipal: 'Flügel',
+			enlaces: [
+				{link: '#/', nombre: 'Inicio'},
+				{link: '#/about', nombre: 'About'}
+			],
+			collapse:[
+				{link: '#/', nombre: 'Ayuda'},
+				{link: '#/about', nombre: 'Cerrar Sesión'}
+			]			
+		};
+		
+	}]);
+
