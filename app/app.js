c35ec85d5b8b9e11146957b58d0e5e32604ecbda
lasdjs <lenninlasd@gmail.com>
2015-03-30 16:19:07 -0500

    integracion con node, utilizacion de bower_components

--- app/app.js
+++ app/app.js
@@ -12,7 +12,8 @@ var pool = mysql.createPool({
 var http = require('http').Server(app);
 
 app.use(cors());
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/flugel'));
+app.use('/old', express.static(__dirname + '/app'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 routes(app, pool);
