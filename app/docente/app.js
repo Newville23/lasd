c24d777d96f68f50263944bfb88ed25403999c70
lasdjs <lenninlasd@gmail.com>
2015-03-31 21:47:43 -0500

    mas integracion

--- app/docente/app.js
+++ app/docente/app.js
@@ -7,8 +7,13 @@ angular.module('CDapp',[
     ])
 
 .config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-    .primaryPalette('indigo')
+    $mdThemingProvider.theme('top')
+    .primaryPalette('grey', {
+      'default': '100', // by default use shade 400 from the gray palette for primary intentions
+      'hue-1': '800', // use shade 100 for the <code>md-hue-1</code> class
+      'hue-2': '700', // use shade 600 for the <code>md-hue-2</code> class
+      'hue-3': '300' // use shade A100 for the <code>md-hue-3</code> class
+    })
     .accentPalette('blue');
 })
 
