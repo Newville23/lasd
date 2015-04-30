97d90db724b28f0d6383e93881e9203e0298e848
zenerlasd <lenninlasd@gmail.com>
2015-04-30 14:13:14 -0500

    inicio put logro

--- app/app/scripts/controllers/contenido.js
+++ app/app/scripts/controllers/contenido.js
@@ -6,27 +6,49 @@ angular.module('Dirapp')
     $scope.logro = {};
 
     Docente.contenido.query({idclase: idClase}, function(logros){
-    	console.log(logros);
     	$scope.Logros = logros;
 
         Docente.calificaciones.query({idclase: idClase}, function(evaluaciones){
             $scope.evaluaciones = evaluaciones;
-            console.log(evaluaciones);
         });
     });
 
-    $scope.saveLogro = function(){
-      $scope.logro.idclase = idClase;
-      Docente.contenido.save($scope.logro, function(log){
-        var logro = {
-            id_indicador: log.insertId,
-            id_clase: log.data.idclase,
-            contenido: log.data.contenido,
-            periodo: log.data.periodo,
-            fecha_vencimiento: log.data.fechavencimiento
-        };
-        console.log(logro);
-        $scope.Logros.push(logro);
-      });
+    $scope.saveLogro = function(id_indicador){
+      
+        if (id_indicador) {
+            if (_.size($scope.logro)) {
+                console.log($scope.logro);
+            }
+
+        }else{
+
+            // POST logro
+            $scope.logro.idclase = idClase;
+            Docente.contenido.save($scope.logro, function(log){
+                var logro = {
+                    id_indicador: log.insertId,
+                    id_clase: log.data.idclase,
+                    contenido: log.data.contenido,
+                    periodo: log.data.periodo,
+                    fecha_vencimiento: log.data.fecha_vencimiento
+                };
+                $scope.Logros.push(logro);
+                _.delay(function(){
+                    $('#modalNewLogro').modal('hide');
+                }, 1000);        
+            });
+        }
+    };
+
+    $scope.launchPostModal = function(){
+        $scope.logro = {};
+    };
+
+    $scope.launchEditModal = function(id_indicador){
+        //console.log(moment().format("ddd, hA"));
+        $scope.logro = _.findWhere($scope.Logros, {id_indicador: id_indicador});
+        if ($scope.logro.fecha_vencimiento) {
+            $scope.logro.fecha_vencimiento = moment(new Date($scope.logro.fecha_vencimiento)).format("YYYY-MM-DD");
+        }
     };
 }]);
